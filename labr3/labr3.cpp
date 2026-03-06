9ce1687c0388423bc21588519862fec7e60e03b8
USER <USER@DESKTOP-K0V9S8M>
2026-03-06 10:10:48 +0200

    Add project files.

---
+++ labr3/labr3.cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int n = 4;
+    double a[n][n];
+
+    int positive = 0;
+    int negative = 0;
+    int zero = 0;
+
+    cout << "Enter matrix elements 4x4:\n";
+
+    // Введення матриці
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    // Підрахунок елементів
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i][j] > 0)
+                positive++;
+            else if (a[i][j] < 0)
+                negative++;
+            else
+                zero++;
+        }
+    }
+
+    // Виведення результатів
+    cout << "Positive elements: " << positive << endl;
+    cout << "Negative elements: " << negative << endl;
+    cout << "Zero elements: " << zero << endl;
+
+    return 0;
+}
